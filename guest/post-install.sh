0a94b754f3b55c175d25ce5cc8cd8b74d4c3c4b6
naripok <fernando.canteruccio@gmail.com>
2023-01-16 20:14:04 -0300

    feat: install yay silently

--- guest/post-install.sh
+++ guest/post-install.sh
@@ -6,7 +6,7 @@ echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 # Install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 
 # Install our stuff
 git clone https://github.com/naripok/.dotfiles.git
